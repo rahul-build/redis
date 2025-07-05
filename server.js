d130a9057a946d4a4d6ff54e42d616fb72dc7751
rahul-build <rahulbuild.dev@gmail.com>
2025-07-05 15:28:08 +0530

    feat:create a new dataStructure file for practics

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ async function testRedisConnection() {
     await client.set("key", "Rahul Kumar");
     const extractvalue = await client.get("key");
     console.log(extractvalue);
+    //Delete
+    const deleteCount = await client.del("key");
+    console.log(deleteCount);
+
+    // Update
+    const extractUpdateValue = await client.get("key");
+    console.log(extractUpdateValue);
+
+    //increment
+
+    await client.set("value", 100);
+    const incremet = await client.incr("value");
+    console.log(incremet);
   } catch (error) {
     console.error(error);
   } finally {
